a9adc204876271d89f746fa03c41e4028393e4c7
Jeffrey Gisin <jgisin@gmail.com>
2016-03-17 14:52:47 -0600

    promises

--- scripts.js
+++ scripts.js
@@ -1,20 +1,51 @@
-var $ = (function() {
+var Promise = (function() {
+
+  return function(typ, trg){
+    this.callbacks = {};
+
+    this.type = typ || 'fx';
+    this.target = trg;
+    var that = this;
+    this.fire = function(fncName, argArray){
+      if(that.callbacks[fncName] !== undefined){
+        that.callbacks[fncName].apply(trg, argArray);
+      }
+    };
+    this.done = function(callback){
+      that.callbacks.done = callback;
+    };
+    this.fail = function(callback){
+      that.callbacks.fail = callback;
+    };
+    this.always = function(callback){
+      that.callbacks.always = callback;
+          console.log('I got called');
+    };
+  };//close promise return
+
+})();
+
+var $ = (function(Promise) {
+
   var xml = new XMLHttpRequest();
 
   var ajax = function(options){
+    var promise = new Promise("fx", xml);
     var returnValue;
-    
+
     xml.addEventListener( "load", function(event){
       options.complete(event.target, event.target.statusText);
+      promise.fire("always", [event.target, event.target.statusText]);
 
       if(event.target.status >= 200 && event.target.status < 300){
         options.success(event.target, event.target.responseText, event.target.statusText);
-        returnValue = promise('fx', event.target);
+        promise.fire("done", [event.target, event.target.responseText, event.target.statusText]);
       }
       else{
         options.error(event.target, event.type, event);
+        promise.fire("fail", [event.target, event.type, event]);
       }
-      
+
     });
     xml.open(options.method, options.url, options.async);
     if (options.headers !== {}) {
@@ -22,35 +53,34 @@ var $ = (function() {
       xml.setRequestHeader(headerKey, options.headers[headerKey]);
     }
     xml.send();
-    return promise('fx', xml);
+    return promise;
   };
 
   var get = function(url, data, callback) {
     options.url = url;
     options.data = data;
     options.method = 'GET';
-    promise = ajax(options);
-    xml.onload = callback(promise.target);
+    var event = ajax(options);
+    xml.onload = callback(event);
   };
 
   var post = function(url, data, callback) {
     options.url = url;
     options.data = data;
     options.method = 'POST';
-    promise = ajax(options);
-    xml.onload = callback(promise.target);
+    var event = ajax(options);
+    xml.onload = callback(event);
   };
 
   return {
     ajax: ajax,
     get: get,
     post: post
-  }
+  };
 
-})();
+})(Promise);
 
 var options = (function() {
-
   var complete = function(xhrObj, statusString){
     console.log('Complete: ' + xhrObj, statusString);
   };
@@ -80,22 +110,10 @@ var options = (function() {
     success: success,
     url: url,
     async: async
-  }
-
-})();
-
-var promise = (function(type, target) {
-  var type = type || 'fx';
-  var target = target;
-
-  var done = function() {
-    console.log('I got called');
   };
 
-  return {
-    target: target,
-    done: done,
-  }
 })();
 
-$.post(options.url, options.data, function(event) {console.log('Callback: ' + event)});
+
+
+$.ajax(options).done(function(a,b,c){console.log(a,b,c);});
