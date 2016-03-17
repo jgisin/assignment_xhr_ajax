00937e81cdca11caf2f6732173d95f76d3858f62
Kelsey James <kelseybjames@gmail.com>
2016-03-17 11:07:23 -0700

    working on promises

--- scripts.js
+++ scripts.js
@@ -1,49 +1,101 @@
-$ = {
+var $ = (function() {
+  var xml = new XMLHttpRequest();
 
-  ajax: function(options){
-    var xml = new XMLHttpRequest();
-    //complete callback
+  var ajax = function(options){
+    var returnValue;
+    
     xml.addEventListener( "load", function(event){
-      console.log(event.target);
+      options.complete(event.target, event.target.statusText);
+
       if(event.target.status >= 200 && event.target.status < 300){
-        options.success(event.target, event.target.statusText);
+        options.success(event.target, event.target.responseText, event.target.statusText);
+        returnValue = promise('fx', event.target);
       }
       else{
         options.error(event.target, event.type, event);
       }
+      
     });
     xml.open(options.method, options.url, options.async);
+    if (options.headers !== {}) {
+      headerKey = Object.keys(options.headers)[0];
+      xml.setRequestHeader(headerKey, options.headers[headerKey]);
+    }
     xml.send();
+    return promise('fx', xml);
+  };
 
-    //error callback
-    // xml.addEventListener( "error", options.error(event.target, event.type, event));
-  },
+  var get = function(url, data, callback) {
+    options.url = url;
+    options.data = data;
+    options.method = 'GET';
+    promise = ajax(options);
+    xml.onload = callback(promise.target);
+  };
 
-};
+  var post = function(url, data, callback) {
+    options.url = url;
+    options.data = data;
+    options.method = 'POST';
+    promise = ajax(options);
+    xml.onload = callback(promise.target);
+  };
 
-options = {
+  return {
+    ajax: ajax,
+    get: get,
+    post: post
+  }
 
-  complete: function(xhrObj, statusString){
-    console.log(xhrObj, statusString);
-  },
+})();
 
-  data: {},
+var options = (function() {
 
-  error: function(xhrObj, errorString, exceptionObj){
-    console.log(xhrObj, errorString, exceptionObj);
-  },
+  var complete = function(xhrObj, statusString){
+    console.log('Complete: ' + xhrObj, statusString);
+  };
 
-  headers: {},
+  var data = {};
 
-  method: "GET",
+  var error = function(xhrObj, errorString, exceptionObj){
+    console.log('Error: ' + xhrObj, errorString, exceptionObj);
+  };
 
-  success: function(xhrObj, data, statusString){
+  var headers = {};
 
-  },
+  var method = "POST";
 
-  url: "http://jsonplaceholder.typicode.com/posts",
+  var success = function(xhrObj, data, statusString){
+    console.log('Success: ' + xhrObj, data, statusString);
+  };
+  var url = "http://jsonplaceholder.typicode.com/posts";
+  var async = true;
 
-  async: true,
+  return {
+    complete: complete,
+    data: data,
+    error: error,
+    headers: headers,
+    method: method,
+    success: success,
+    url: url,
+    async: async
+  }
 
-};
-$.ajax(options);
+})();
+
+var promise = (function(type, target) {
+  var type = type || 'fx';
+  var target = target;
+
+  var done = function() {
+    console.log('I got called');
+  };
+
+  return {
+    target: target,
+    done: done,
+  }
+})();
+
+$.post(options.url, options.data, function(event) {console.log('Callback: ' + event)});
